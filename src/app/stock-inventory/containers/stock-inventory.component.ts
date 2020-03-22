a4a7842282b9f28fb9bc32d81cfa3e7163b99f52
ersaumya <ersaumyarout@gmail.com>
2020-03-22 10:47:55 +0530

    added formbuilder

--- src/app/stock-inventory/containers/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 import { Product } from '../models/product.interface';
 
 @Component({
@@ -17,27 +17,27 @@ export class StockInventoryComponent implements OnInit {
     { "id": 5, "price": 28000, "name": "Asus Notebook" },
   ];
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
 
-  form=new FormGroup({
-    store:new FormGroup({
-      branch:new FormControl(''),
-      code:new FormControl('')
+  form=this.fb.group({
+    store:this.fb.group({
+      branch:'',
+      code:''
     }),
     selector:this.createStock({}),
-    stock:new FormArray([
+    stock:this.fb.array([
       this.createStock({product_id:1,quantity:10}),
       this.createStock({ product_id: 3, quantity: 40 })
     ])
   })
 
   createStock(stock){
-    return new FormGroup({
-      product_id:new FormControl(parseInt(stock.product_id,10) || ''),
-      quantity:new FormControl(stock.quantity || 10)
+    return this.fb.group({
+      product_id:parseInt(stock.product_id,10) || '',
+      quantity:stock.quantity || 10
     });
   }
 
