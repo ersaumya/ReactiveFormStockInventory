77db5c35253525cb7b938654a25180440897371f
ersaumya <ersaumyarout@gmail.com>
2020-03-31 20:34:19 +0530

    custom form component

--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -5,11 +5,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { StockBranchComponent } from './components/stock-branch/stock-branch.component';
 import { StockSelectorComponent } from './components/stock-selector/stock-selector.component';
 import { StockProductsComponent } from './components/stock-products/stock-products.component';
+import { StockCounterComponent } from './components/stock-counter/stock-counter.component';
 
 
 
 @NgModule({
-  declarations: [StockInventoryComponent, StockBranchComponent, StockSelectorComponent, StockProductsComponent],
+  declarations: [StockInventoryComponent, StockBranchComponent, StockSelectorComponent, StockProductsComponent, StockCounterComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule
