6e7ef761c18fe1cf93a8c67baffb9836357d082f
ersaumya <ersaumyarout@gmail.com>
2020-04-03 18:10:51 +0530

    control value accessor added

--- src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
+++ src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
@@ -1,11 +1,32 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, forwardRef } from "@angular/core";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+
+const COUNTER_CONTROL_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => StockCounterComponent),
+  multi: true
+};
 
 @Component({
   selector: "app-stock-counter",
   templateUrl: "./stock-counter.component.html",
-  styleUrls: ["./stock-counter.component.scss"]
+  styleUrls: ["./stock-counter.component.scss"],
+  providers: [COUNTER_CONTROL_ACCESSOR]
 })
-export class StockCounterComponent implements OnInit {
+export class StockCounterComponent implements OnInit, ControlValueAccessor {
+  private onTouch: Function;
+  private onModelChange: Function;
+
+  writeValue(value) {
+    this.value = value || 0;
+  }
+  registerOnChange(fn) {
+    this.onModelChange = fn;
+  }
+  registerOnTouched(fn) {
+    this.onTouch = fn;
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
@@ -18,12 +39,16 @@ export class StockCounterComponent implements OnInit {
   increment() {
     if (this.value < this.max) {
       this.value = this.value + this.step;
+      this.onModelChange(this.value);
     }
+    this.onTouch();
   }
 
   decrement() {
     if (this.value > this.min) {
       this.value = this.value - this.step;
+      this.onModelChange(this.value);
     }
+    this.onTouch();
   }
 }
