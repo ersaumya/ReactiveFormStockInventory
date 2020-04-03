c9d95e57323fe12062532d6328eb6f69b23c3b8c
ersaumya <ersaumyarout@gmail.com>
2020-04-03 18:47:53 +0530

    eyeboard events

--- src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
+++ src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
@@ -35,6 +35,34 @@ export class StockCounterComponent implements OnInit, ControlValueAccessor {
   @Input() max: number = 1000;
 
   value: number = 10;
+  focus: boolean;
+
+  onKeyDown(event: KeyboardEvent) {
+    const handlers = {
+      ArrowDown: () => this.decrement(),
+      ArrowUp: () => this.increment()
+    };
+    if (handlers[event.code]) {
+      handlers[event.code]();
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    this.onTouch();
+  }
+
+  onBlur(event: FocusEvent) {
+    this.focus = false;
+     event.preventDefault();
+     event.stopPropagation();
+     this.onTouch();
+  }
+
+  onFocus(event: FocusEvent) {
+    this.focus = true;
+     event.preventDefault();
+     event.stopPropagation();
+     this.onTouch();
+  }
 
   increment() {
     if (this.value < this.max) {
